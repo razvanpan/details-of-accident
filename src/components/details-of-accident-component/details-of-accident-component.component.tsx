d46282a57d587b9bd8018fe7e53b0bfe676ade9b
Lidia <mslydka@gmail.com>
2020-09-11 14:18:20 +0300

    Added details of accident component

---
+++ src/components/details-of-accident-component/details-of-accident-component.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Body from '../body-component/body-component.component'
+import Header from '../header-component/header-component.component'
+import Footer from '../footer-component/footer-component.component'
+
+const DetailsOfAccident = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default DetailsOfAccident
