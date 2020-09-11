e4455901ffb3a2a1f44aca9e0c27243e2f0026ce
Lidia <mslydka@gmail.com>
2020-09-11 14:17:48 +0300

    Added header component

---
+++ src/components/header-component/header-component.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ActionHeader from 'terra-action-header'
+
+const Header = () => (
+  <div>
+    <br />
+    <ActionHeader title='Default Action Header' />
+    <br />
+  </div>
+)
+export default Header
