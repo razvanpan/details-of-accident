972d04291fd748add0467734665e0f73639121ac
Lidia <mslydka@gmail.com>
2020-09-14 15:52:52 +0300

    Edited the structure of components folder and renamed components properly

---
+++ src/components/details-of-accident.component.tsx
@@ -0,0 +1,16 @@
+import React from './body/node_modules/react'
+import Body from './body/body.component'
+import Header from './header/header.component'
+import Footer from './footer/footer.component'
+
+const DetailsOfAccident = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default DetailsOfAccident
