e47c6d9f6087506f37728fabe1cf7c116a92a4da
Lidia <mslydka@gmail.com>
2020-09-29 15:12:33 +0300

    implemented clinical-view component

--- src/components/body/body.component.tsx
+++ src/components/body/body.component.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import DetailViewDivided from '../clinical-detail/clinical-detail.component'
 
 const DetailsOfAccidentBody = () => (
-  <div className='body-details-of-accident'></div>
+  <div
+    className='body-details-of-accident'
+    style={{
+      display: 'flex'
+    }}
+  >
+    <DetailViewDivided />
+  </div>
 )
 
 export default DetailsOfAccidentBody
---
+++ src/components/clinical-detail/clinical-detail.component.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import LabelValueView from 'terra-clinical-label-value-view'
+import DetailView from 'terra-clinical-detail-view'
+
+const dateItem = <LabelValueView label='Date' textValue='26/06/2019' />
+const locationItem = (
+  <LabelValueView label='Location' textValue='Goethestrasse 12' />
+)
+const typeItem = <LabelValueView label='Type' textValue='Car Accident' />
+const helmetItem = <LabelValueView label='Helmet' textValue='Yes' />
+const mechanismItem = <LabelValueView label='Mechanism' textValue='Blunt' />
+const DetailViewDivided = () => (
+  <div>
+    <DetailView
+      details={[
+        <DetailView.DetailList key='order-info-2'>
+          <DetailView.DetailListItem item={dateItem} />
+          <DetailView.DetailListItem item={locationItem} />
+          <DetailView.DetailListItem item={typeItem} />
+          <DetailView.DetailListItem item={helmetItem} />
+          <DetailView.DetailListItem item={mechanismItem} />
+        </DetailView.DetailList>
+      ]}
+      isDivided={false}
+    />
+  </div>
+)
+export default DetailViewDivided
